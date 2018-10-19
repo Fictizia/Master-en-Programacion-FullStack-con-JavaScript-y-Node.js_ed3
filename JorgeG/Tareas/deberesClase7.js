4bd4e5909aa13f896b490b7e4c1f87d34a33d765
Jorge Guijarro <43820446+JGuijarro@users.noreply.github.com>
2018-10-19 21:52:43 +0200

    Create deberesClase7.js

---
+++ JorgeG/Tareas/deberesClase7.js
@@ -0,0 +1,27 @@
+// Ejercicio 2
+
+// Usando for
+
+for (var n = 0; n < 100; n++) {
+    console.log(n);
+}
+
+// Usando while y break
+
+var n = 1;
+while (n <= 100) {
+    console.log(n);
+    n++;
+}
+
+if (n === 100) { 
+    	break; 
+    }
+
+// Usando do..while
+
+var n = 0;
+do {
+   n++;
+   console.log(n);
+} while (n < 100);
