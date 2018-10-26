df4fb7f22116969736b502b6fafe17328c21f5db
Jorge Guijarro <43820446+JGuijarro@users.noreply.github.com>
2018-10-26 20:33:08 +0200

    Update practicaVending.js

--- JorgeG/practicas/practicaVending.js
+++ JorgeG/practicas/practicaVending.js
@@ -1,1 +1,1 @@
-https://github.com/Fictizia/Master-en-Programacion-FullStack-con-JavaScript-y-Node.js_ed3/blob/master/teoria/clase11.md
+<a href="https://github.com/Fictizia/Master-en-Programacion-FullStack-con-JavaScript-y-Node.js_ed3/blob/master/teoria/clase11.md">Enunciado (Clase 11)</a>
