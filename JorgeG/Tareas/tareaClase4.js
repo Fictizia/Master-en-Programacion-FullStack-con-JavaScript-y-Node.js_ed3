9c74b43087bec7d2a87ae8364c6e43163dae4210
Jorge Guijarro <43820446+JGuijarro@users.noreply.github.com>
2018-12-27 22:17:22 +0100

    Create tareaClase4.js

---
+++ JorgeG/Tareas/tareaClase4.js
@@ -0,0 +1,15 @@
+// 5 -tabla de posiciones de compañeros (de izquierda a derecha, de delante hacia atrás)
+
+var claseMasterJs = [
+     { nombre: "Ulises", posicion: "0"},
+     { nombre: "Jorge", posicion: "1" },
+     { nombre: "Guille", posicion: "2" },
+     { nombre: "Teba", posicion: "3" },
+     { nombre: "Karina", posicion: "4"},
+     { nombre: "Eva", posicion: "5"},
+     { nombre: "Fabiola", posicion: "6"}
+ ];
+ 
+console.table(claseMasterJs);
+ 
+console.table(claseMasterJs, "posicion");
