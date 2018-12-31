6003d46248cb391dedcb910650a04a19f7234493
Jorge Guijarro <43820446+JGuijarro@users.noreply.github.com>
2018-12-31 18:43:01 +0100

    Create tareaClase3.js

---
+++ JorgeG/Tareas/tareaClase3.js
@@ -0,0 +1,19 @@
+// 1 - programa que imprime los numeros del 1 al 100
+
+for (i = 0; i < 100; i++){
+  console.log(i)
+  }
+  
+// 2 - programa que muestra los numeros del 100 al 0
+
+for (i = 100; i >= 0; i--){
+  console.info(i)
+  }
+  
+ // 3 - diseña un programa que muestre los numeros pares entre 0 y 100
+ 
+ for (i = 0; i < 100; i++){
+  if (i%2 === 0){
+  console.info(i)
+    }
+  }
