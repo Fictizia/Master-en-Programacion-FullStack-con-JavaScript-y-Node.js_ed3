fb2dd8fa1d4dff77e88da2e623b3c5727e436fec
Jorge Guijarro <43820446+JGuijarro@users.noreply.github.com>
2019-01-01 23:47:02 +0100

    Update tareaClase4.js

--- JorgeG/Tareas/tareaClase4.js
+++ JorgeG/Tareas/tareaClase4.js
@@ -1,3 +1,19 @@
+// 1 - Utiliza .assert para controlar cuando se muestra tu nombre por consola.
+
+
+/* 2 - Crea un script que nos permita calcular el tiempo que se necesita para realizar la siguientes acciones.
+Especificaciones:
+Imprimir tu nombre usando console.log, console.info, console.warn
+*/
+
+
+// 3 - Partiendo del ejercicio anterior mejora los estilos de cada mensaje usando estilos.
+
+
+// 4 - Agrupa cada tipo de mensaje y añade un contador de tiempo por cada grupo.
+
+
+
 // 5 -tabla de posiciones de compañeros (de izquierda a derecha, de delante hacia atrás)
 
 var claseMasterJs = [
