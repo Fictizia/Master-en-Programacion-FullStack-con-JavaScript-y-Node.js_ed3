2a5cedc55e7415bfbf959b169c194e56a74faaf1
Jorge Guijarro <43820446+JGuijarro@users.noreply.github.com>
2019-01-01 23:54:04 +0100

    Update tareaClase3.js

--- JorgeG/Tareas/tareaClase3.js
+++ JorgeG/Tareas/tareaClase3.js
@@ -1,20 +1,20 @@
 // 1 - programa que imprime los numeros del 1 al 100
 
 for (i = 0; i < 100; i++){
-  console.log(i)
+  console.log(i);
   }
   
 // 2 - programa que muestra los numeros del 100 al 0
 
 for (i = 100; i >= 0; i--){
-  console.info(i)
+  console.info(i);
   }
   
  // 3 - diseña un programa que muestre los numeros pares entre 0 y 100
  
  for (i = 0; i < 100; i++){
   if (i%2 === 0){
-  console.info(i)
+  console.info(i);
     }
   }
 
@@ -22,35 +22,37 @@ for (i = 100; i >= 0; i--){
 
   var coin = Math.round(Math.random()*(1 -0) + 0);
   if (coin === 1){
-    console.log("ha salido cara")
+    console.log("ha salido cara");
   }
   else {
-    console.log("ha salido cruz")
+    console.log("ha salido cruz");
   }
 
 // 5 - diseña un programa que simula cien tiradas de dos dados y contar las veces que entre los dos suman 10
 
-var dice1 = Math.round(Math.random()*(6 -1) + 1)
-var dice2 = Math.round(Math.random()*(6 -1) + 1)
-var coincidences = 0
+var dice1 = Math.round(Math.random()*(6 -1) + 1);
+var dice2 = Math.round(Math.random()*(6 -1) + 1);
+var coincidences = 0;
 
-function roll2Dices (dice1; dice2)
+function roll2Dices (dice1; dice2) {
   for (i = 0; i < 100; i++){
-    if (num1 + num2 = 10)
-      coincidences += 1
+    if (dice1 + dice2 = 10){
+      coincidences += 1;
     }
+    }
+  }
 
 // 6 - Diseña un programa para calcular el porcentaje de hombres y mujeres en nuestro curso
 
-var nummen = prompt("introduce el numero de hombres")
-var numwomen = prompt("introduce el numero de mujeres")
-var total = nummen + numwomen
+var nummen = prompt("introduce el numero de hombres");
+var numwomen = prompt("introduce el numero de mujeres");
+var total = nummen + numwomen;
 
-var pctmen = nummen*100 / total
-var pctwomen = numwoman*100 / total
+var pctmen = nummen*100 / total;
+var pctwomen = numwoman*100 / total;
 
-console.log("el procentaje de hombres es" + pctmen)
-console.log("el porcentaje de mujeres es" + pctwomen)
+console.log("el procentaje de hombres es" + pctmen);
+console.log("el porcentaje de mujeres es" + pctwomen);
 
 
 /* 7 - Diseña un algoritmo que aplique al precio de un producto un descuento cuando se den las siguientes caracteristicas:
@@ -59,19 +61,19 @@ Estamos en los meses de invierno
 Y no es viernes o fin de semana.
 */
 
-var month = prompt("escribe el mes en el que estamos")
-var day = prompt("escribe el día de la semana que es hoy")
-var price = prompt("escribe el precio del producto")
+var month = prompt("escribe el mes en el que estamos");
+var day = prompt("escribe el día de la semana que es hoy");
+var price = prompt("escribe el precio del producto");
 var dicountedPrice = price*0.75
 
-month = month.toLowerCase()
-day = day.toLowerCase()
+month = month.toLowerCase();
+day = day.toLowerCase();
 
 if (((month === "enero") || (month === "febrero") || (month === "marzo")) && ((day !== "viernes") && (day !== "sabado") && (weekday !== "domingo"))){
   console.log("Precio rebajado!" + discountedPrice)
 }
   else {
-    console.log("Precio normal" + price)
+    console.log("Precio normal" + price);
   }
 
 
