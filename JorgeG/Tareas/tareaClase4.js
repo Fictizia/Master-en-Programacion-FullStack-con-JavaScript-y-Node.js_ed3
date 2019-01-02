6f62cb50fa1d0989e32a5ebdcf3d906500f6b9fa
Jorge Guijarro <43820446+JGuijarro@users.noreply.github.com>
2019-01-02 18:04:43 +0100

    Update tareaClase4.js

--- JorgeG/Tareas/tareaClase4.js
+++ JorgeG/Tareas/tareaClase4.js
@@ -1,9 +1,9 @@
 // 1 - Utiliza .assert para controlar cuando se muestra tu nombre por consola.
 
-var nombre = "Jorge";
+var name = "Jorge";
 console.assert(nombre === "Jorge", "Right name");
 
-var nombre = "Paco";
+var name = "Paco";
 console.assert(nombre === "Jorge", "Wrong name");
 
 /* 2 - Crea un script que nos permita calcular el tiempo que se necesita para realizar la siguientes acciones.
@@ -11,17 +11,26 @@ Especificaciones:
 Imprimir tu nombre usando console.log, console.info, console.warn
 */
 
-console.time("Medición de la impresión de mi nombre");
+console.time("Medición de la impresión de mi nombre iniciada");
 
-console.log("Jorge");
-console.info("Jorge");
-console.warn("Jorge");
+var name = "Jorge";
+console.log(name);
+console.info(name);
+console.warn(name);
 
-console.timeEnd("Medición de miArray");
+console.timeEnd("Medición de la impresión de mi nombre finalizada");
 
 
 // 3 - Partiendo del ejercicio anterior mejora los estilos de cada mensaje usando estilos.
 
+
+console.time("Medición de la impresión de mi nombre iniciada");
+
+console.log("%c Jorge", "background: #16A085; color: #FFF; font-size:25px;");
+console.info("%c Jorge", "background: #8E44AD; color: #FFF; font-size:25px;");
+console.warn("%c Jorge", "background: #C0392B; color: #FFF; font-size:25px;");
+
+console.timeEnd("Medición de la impresión de mi nombre finalizada");
 
 // 4 - Agrupa cada tipo de mensaje y añade un contador de tiempo por cada grupo.
 
