680f9c3db0a0f04727c43de99fe9c9cefe74b7d4
Jorge Guijarro <43820446+JGuijarro@users.noreply.github.com>
2019-01-02 19:38:20 +0100

    Update tareaClase5.js

--- JorgeG/Tareas/tareaClase5.js
+++ JorgeG/Tareas/tareaClase5.js
@@ -1,17 +1,25 @@
 /*
-
 let num1 = Math.floor(math.random()*();
 let num2 = Math.floor(math.random());
-
 */
 
-// Ejercicio2
-
+/*1 - Diseña un algoritmo que lea dos números y realice los siguientes cálculos:
+Valor de su suma
+Valor de su resta
+Valor de su division
+Valor de su producto
+*/
 let num1 = prompt("introduzca un numero", "");
 let num2 = prompt("introduzca un numero", "");
-console.info("el resultado es" + (num1 + num2));
 
-// Ejercicio3
+function basicCalculations(num1, num2){
+console.info("el resultado de la suma es " + (num1 + num2);
+console.info("el resultado de la resta es " + (num1 - num2);
+console.info("el resultado de la multiplicacion es " + (num1 * num2);
+console.info("el resultado de la division es " + (num1 / num2);
+             }
+
+//2 - Diseña un algoritmo para calcular el porcentaje de hombres y mujeres en nuestro curso.
 
 let nummen = prompt("introduzca el numero de hombres", "");
 let numwom = prompt("introduzca el numero de mujeres", "");
