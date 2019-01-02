6ec5532144175b38466ac7bf68400f2aa42ed5b2
Jorge Guijarro <43820446+JGuijarro@users.noreply.github.com>
2019-01-02 21:24:26 +0100

    Update tareaClase4.js

--- JorgeG/Tareas/tareaClase4.js
+++ JorgeG/Tareas/tareaClase4.js
@@ -1,4 +1,4 @@
-// 1 - Utiliza .assert para controlar cuando se muestra tu nombre por consola.
+//1 - Utiliza .assert para controlar cuando se muestra tu nombre por consola.
 
 var name = "Jorge";
 console.assert(name === "Jorge", "Right name");
@@ -6,7 +6,7 @@ console.assert(name === "Jorge", "Right name");
 var name = "Paco";
 console.assert(name === "Jorge", "Wrong name");
 
-/* 2 - Crea un script que nos permita calcular el tiempo que se necesita para realizar la siguientes acciones.
+/*2 - Crea un script que nos permita calcular el tiempo que se necesita para realizar la siguientes acciones.
 Especificaciones:
 Imprimir tu nombre usando console.log, console.info, console.warn
 */
@@ -21,7 +21,7 @@ console.warn(name);
 console.timeEnd("Medición de la impresión de mi nombre");
 
 
-// 3 - Partiendo del ejercicio anterior mejora los estilos de cada mensaje usando estilos.
+//3 - Partiendo del ejercicio anterior mejora los estilos de cada mensaje usando estilos.
 
 
 console.time("Medición de la impresión de mi nombre");
@@ -34,7 +34,7 @@ console.warn("%c" + name, "background: #C0392B; color: #FFF; font-size:25px;");
 
 console.timeEnd("Medición de la impresión de mi nombre");
 
-// 4 - Agrupa cada tipo de mensaje y añade un contador de tiempo por cada grupo.
+//4 - Agrupa cada tipo de mensaje y añade un contador de tiempo por cada grupo.
 
 console.group("logGroup");
 console.time("logTime");
@@ -54,7 +54,7 @@ console.warn("%c Jorge", "background: #C0392B; color: #FFF; font-size:25px;");
 console.timeEnd("warnTime");
 console.groupEnd("warnGroup");
 
-// 5 -tabla de posiciones de compañeros (de izquierda a derecha, de delante hacia atrás)
+//5 -tabla de posiciones de compañeros (de izquierda a derecha, de delante hacia atrás)
 
 var claseMasterJs = [
      { nombre: "Ulises", posicion: "0"},
