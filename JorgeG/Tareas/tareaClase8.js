2bb4891994175588117d071a822f0d2330052ae3
Jorge Guijarro <43820446+JGuijarro@users.noreply.github.com>
2019-01-04 02:04:59 +0100

    Create tareaClase8.js

---
+++ JorgeG/Tareas/tareaClase8.js
@@ -0,0 +1,15 @@
+//1 - Diseña un algoritmo que simula el lanzamiento de una moneda al aire e imprimir si ha salido cara o cruz.
+
+//2 - Diseña un algoritmo que simula cien tiradas de dos dados y contar las veces que entre los dos suman 10.
+
+//3 - Diseña un script que confirme si una fecha es valida y además devuelva la fecha en dos formatos diferentes.
+
+//4 - ¿Que fecha será dentro de 30 días?
+
+//5 - ¿Cuantas horas han pasado desde que emepezó este master? y... ¿en días?
+
+//6 - ¿Cuantos milisengundos quedan para terminar el master? y... ¿en horas o días?
+
+//7 - ¿Que fecha será dentro de un año y 10 horas más?
+
+//8 - Imprimir por consola la fecha completa (formato texto) en koreano y japones.
