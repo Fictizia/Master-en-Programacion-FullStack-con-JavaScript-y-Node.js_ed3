927692fd9b2ee2f594d8a020bac7946841eab4b9
KoolTheba <26543236+KoolTheba@users.noreply.github.com>
2019-05-20 20:47:26 +0200

    improved routes, body parser effects and validator checks

--- Teba/MovieFire_ApiRest/server.js
+++ Teba/MovieFire_ApiRest/server.js
@@ -1,38 +1,63 @@
-const express = require('express');
-const {port} = require("./config");
-const app = express();
-const bodyParser = require('body-parser');
+const express = require('express')
+const firebase = require('firebase')
+const { port } = require('./config')
+const { getAllMovies, getMovieDetails } = require('./controllers/api')
+const { pushFilmDB, filmDetailsDB, editFilmDetailsDB, deleteFilmDB } = require('./models/films')
+const app = express()
+const db = firebase()
+const bodyParser = require('body-parser')
+const { check, validationResult } = require('express-validator/check')
 
-app.use(express.static('public'));
+app.use(express.static('public'))
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-    res.sendFile('./public/index/html');
-  });
+  res.sendFile('./public/index/html')
+})
 
-app.get('/api/movies', (req, res, next) => {
-    res.json([{id: "01"}, {id: "02"}]);
-});
+app.get('/api/movies', getAllMovies)
 
-app.get('/api/movies/:id', (req, res, next) => {
-    res.json({id: req.params.id});
-});
+app.get('/api/movies/:id', getMovieDetails)
 
-app.post('/api/movies', (req, res, next) => {
-    let title = req.body.title;
-    res.status(200).send({ title });
-});
+// TODO: propagar esto al resto (no sé cómo hacerlo en post, put, delete)
+app.post('/api/movies', [
+  check('title').isLength({ min: 1 }).trim()
+], (req, res, next) => {
+  let title = req.body.title
+  // Finds the validation errors in this request and wraps them in an object with handy functions
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).send({ errors: 'Missing info!' })
+  } else {
+    res.status(200).send({ title })
+  }
+})
 
-app.put('/api/movies', (req, res, next) => {
-    let {title, id} = req.body
-    res.status(200).send({title, id});
-});
+app.put('/api/movies', [
+  check('title').isLength({ min: 1 }).trim(),
+  check('id').isLength({ min: 1 }).trim()
+], (req, res, next) => {
+  let { title, id } = req.body
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).send({ errors: 'Missing info!' })
+  } else {
+    res.status(200).send({ title, id })
+  }
+})
 
-app.delete('/api/movies', (req, res, next) => {
-    let id = req.body.id;
-    res.status(200).send({ id });
-});
+app.delete('/api/movies', [
+  check('id').isLength({ min: 1 }).trim()
+], (req, res, next) => {
+  let id = req.body.id
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).send({ errors: 'Missing info!' })
+  } else {
+    res.status(200).send({ id })
+  }
+})
 
 app.listen(port, () => {
-    console.log(`Listening to port ${port}`);
+  console.log(`Listening to port ${port}`)
 })
