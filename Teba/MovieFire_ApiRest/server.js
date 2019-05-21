ec1b038f970473ea8538ad920e929563f90a2708
KoolTheba <gomez.teba@gmail.com>
2019-05-21 15:14:21 +0200

    controllers refactor + omdb added

--- Teba/MovieFire_ApiRest/server.js
+++ Teba/MovieFire_ApiRest/server.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const { port } = require('./config')
 const { getAllMovies, getMovieDetails, editMovieDetails, addNewMovie, deleteMovie } = require('./controllers/api')
-const app = express();
+const app = express()
 const bodyParser = require('body-parser')
-const validator = require('validator')
-
 
 /* ---- @TODO ----
 - [x] Eliminar express-validator/check por validator normal.
@@ -12,8 +10,9 @@ const validator = require('validator')
 - [x] firebase solo puede existir en ./models/db
 - [x] fetch no existe en Node! Ver ./models/db
 - [x] './controllers/api' deberia importarse './models/db' para hacer que las rutas sean realmente dinámicas
-- [ ] Desarrollar el gestor de OMBD para sacar más info de las películas desde el title 
-      en ./lib/ombd.js, puedes usar got (Npm), request (Npm) o request-promise (Npm) 
+- [x] './controllers/api' deberia conectarse con './models/db'
+- [x] Desarrollar el gestor de OMBD para sacar más info de las películas desde el title
+      en ./lib/ombd.js, puedes usar got (Npm), request (Npm) o request-promise (Npm)
 - [ ] Mejorar la gestión de errores con un middelware propio (Hablar con @Ulises)
 - [ ] Añadir CORS a las rutas de la /api/ directamente o con el middelware cors (Npm)
 - [ ] Limpiar/borrar/adaptar el front de /public o dejar vacío sin más que un HTML que explique la API
@@ -32,9 +31,9 @@ app.get('/api/movies/:id', getMovieDetails)
 
 app.get('/api/movies', getAllMovies)
 
-app.post('/api/movies', editMovieDetails)
+app.post('/api/movies', addNewMovie)
 
-app.put('/api/movies', addNewMovie)
+app.put('/api/movies', editMovieDetails)
 
 app.delete('/api/movies', deleteMovie)
 
