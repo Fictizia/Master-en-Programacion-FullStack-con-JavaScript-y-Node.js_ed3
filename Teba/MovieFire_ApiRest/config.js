4fe8a6c88b4f30e8032cdaf675f62df462a5c822
KoolTheba <gomez.teba@gmail.com>
2019-05-21 12:36:53 +0200

    firebase config added

--- Teba/MovieFire_ApiRest/config.js
+++ Teba/MovieFire_ApiRest/config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  port: 3000
+  port: process.env.PORT || 3000,
+  firebasePath: "moviefire-apirest",
+  firebaseConfig: {
+    apiKey: "AIzaSyAp5SKIZPqD267iqov7LHmcrE11ccnmb20",
+    authDomain: "moviefire-3bf94.firebaseapp.com",
+    databaseURL: "https://moviefire-3bf94.firebaseio.com",
+    projectId: "moviefire-3bf94",
+    storageBucket: "moviefire-3bf94.appspot.com",
+    messagingSenderId: "243777350623",
+    appId: "1:243777350623:web:8e0f7589641cfa1b"
+  }
 }
