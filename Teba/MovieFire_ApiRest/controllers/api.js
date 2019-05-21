c3ede32b290b02940e7ec1df6f4ce4ac5e1d62df
KoolTheba <gomez.teba@gmail.com>
2019-05-21 12:38:26 +0200

    clean routes

--- Teba/MovieFire_ApiRest/controllers/api.js
+++ Teba/MovieFire_ApiRest/controllers/api.js
@@ -1,10 +1,45 @@
 
+const { getAllFilmsDB, filmDetailsDB, createFilmDB, editFilmDetailsDB, deleteFilmDB } = require('../models/db')
+
 function getAllMovies (req, res, next) {
-  res.json([{ id: '01' }, { id: '02' }])
+  getAllFilmsDB()
+  .then(data => res.json(data))
+  .catch(err => res.status(500).json(err))  
 }
 
 function getMovieDetails (req, res, next) {
   res.json({ id: req.params.id })
 }
 
-module.exports = { getAllMovies, getMovieDetails }
+function editMovieDetails (req, res, next) {
+  let title = req.body.title
+  const isValidTitle = title && validator.isLength(title.trim(), { min: 1 })
+  if (!isValidTitle) {
+    return res.status(400).send({ errors: 'Missing info!' })
+  } else {
+    res.status(200).send({ title })
+  }
+}
+
+function addNewMovie (req, res, next) {
+  let { title, id } = req.body;
+  const isValidTitle = title && validator.isLength(title.trim(), { min: 1 })
+  const isValidId = id && validator.isLength(id.trim(), { min: 1 })
+  if (!isValidTitle && !isValidId) {
+    return res.status(400).send({ errors: 'Missing info!' })
+  } else {
+    res.status(200).send({ title, id })
+  }
+}
+
+function deleteMovie (req, res, next) {
+  let id = req.body.id;
+  const isValidId = id && validator.isLength(id.trim(), { min: 1 })
+  if (!isValidId) {
+    return res.status(400).send({ errors: 'Missing info!' })
+  } else {
+    res.status(200).send({ id })
+  }
+}
+
+module.exports = { getAllMovies, getMovieDetails, editMovieDetails, addNewMovie, deleteMovie }
--- Teba/MovieFire_ApiRest/server.js
+++ Teba/MovieFire_ApiRest/server.js
@@ -1,22 +1,19 @@
 const express = require('express')
-// const firebase = require('firebase');
 const { port } = require('./config')
-const { getAllMovies, getMovieDetails } = require('./controllers/api')
-// const {pushFilmDB,  filmDetailsDB, editFilmDetailsDB, deleteFilmDB} = require('./models/films')
-const app = express()
-// const db = firebase();
+const { getAllMovies, getMovieDetails, editMovieDetails, addNewMovie, deleteMovie } = require('./controllers/api')
+const app = express();
 const bodyParser = require('body-parser')
-const { check, validationResult } = require('express-validator/check')
 const validator = require('validator')
 
 
 /* ---- @TODO ----
-- [ ] Eliminar express-validator/check por validator normal.
-- [ ] Mover el middelware de las rutas a ./controllers/api
-- [ ] firebase solo puede existir en ./models/films
-- [ ] fetch no existe en Node! ver ./models/films
-- [ ] './controllers/api' deberia importarse './models/films' para hacer que las rutas sean realmente dinámicas
-- [ ] Desarrollar el gestor de OMBD para sacar más info de las películas desde el title en ./lib/ombd.js, puedes usar got (Npm), request (Npm) o request-promise (Npm) 
+- [x] Eliminar express-validator/check por validator normal.
+- [x] Mover el middelware de las rutas a ./controllers/api
+- [x] firebase solo puede existir en ./models/db
+- [x] fetch no existe en Node! Ver ./models/db
+- [x] './controllers/api' deberia importarse './models/db' para hacer que las rutas sean realmente dinámicas
+- [ ] Desarrollar el gestor de OMBD para sacar más info de las películas desde el title 
+      en ./lib/ombd.js, puedes usar got (Npm), request (Npm) o request-promise (Npm) 
 - [ ] Mejorar la gestión de errores con un middelware propio (Hablar con @Ulises)
 - [ ] Añadir CORS a las rutas de la /api/ directamente o con el middelware cors (Npm)
 - [ ] Limpiar/borrar/adaptar el front de /public o dejar vacío sin más que un HTML que explique la API
@@ -35,44 +32,11 @@ app.get('/api/movies/:id', getMovieDetails)
 
 app.get('/api/movies', getAllMovies)
 
-app.post('/api/movies', (req, res, next) => {
-  // heck('title').isLength({ min: 1 }).trim()
-  let title = req.body.title
-  const isValidTitle = validator.isLength(title.trim(), { min: 1 })
-  // Finds the validation errors in this request and wraps them in an object with handy functions
-  //const errors = validationResult(req)
-  if (!isValidTitle) {
-    return res.status(400).send({ errors: 'Missing info!' })
-  } else {
-    res.status(200).send({ title })
-  }
-})
+app.post('/api/movies', editMovieDetails)
 
-// TODO: propagar esto al resto (no sé cómo hacerlo en post, put, delete)
-app.put('/api/movies', [
-  check('title').isLength({ min: 1 }).trim(),
-  check('id').isLength({ min: 1 }).trim()
-], (req, res, next) => {
-  let { title, id } = req.body
-  const errors = validationResult(req)
-  if (!errors.isEmpty()) {
-    return res.status(400).send({ errors: 'Missing info!' })
-  } else {
-    res.status(200).send({ title, id })
-  }
-})
+app.put('/api/movies', addNewMovie)
 
-app.delete('/api/movies', [
-  check('id').isLength({ min: 1 }).trim()
-], (req, res, next) => {
-  let id = req.body.id
-  const errors = validationResult(req)
-  if (!errors.isEmpty()) {
-    return res.status(400).send({ errors: 'Missing info!' })
-  } else {
-    res.status(200).send({ id })
-  }
-})
+app.delete('/api/movies', deleteMovie)
 
 app.listen(port, () => {
   console.log(`Listening to port ${port}`)
