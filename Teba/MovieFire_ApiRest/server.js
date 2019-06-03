45a564e88acc5c11794dc6a19fe2669fdce5f152
KoolTheba <gomez.teba@gmail.com>
2019-06-03 14:03:19 +0200

    Lint fix

--- Teba/MovieFire_ApiRest/server.js
+++ Teba/MovieFire_ApiRest/server.js
@@ -6,27 +6,6 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const helmet = require('helmet')
 
-/* ---- @TODO ----
-- [x] Eliminar express-validator/check por validator normal.
-- [x] Mover el middelware de las rutas a ./controllers/api
-- [x] firebase solo puede existir en ./models/db
-- [x] fetch no existe en Node! Ver ./models/db
-- [x] './controllers/api' deberia importarse './models/db' para hacer que las rutas sean realmente dinámicas
-- [x] './controllers/api' deberia conectarse con './models/db'
-- [x] Desarrollar el gestor de OMBD para sacar más info de las películas desde el title
-      en ./lib/ombd.js, puedes usar got (Npm), request (Npm) o request-promise (Npm)
-- [x] Limpiar/borrar/adaptar el front de /public o dejar vacío sin más que un HTML que explique la API
-    - [x] Crear una copia de /public; 
-    - [x] Mapear las funcionalidades de app.js
-    - [x] Migrar las func. de app.js al nuevo app.js siguiendo el esquema de funciones y haciendo las llamadas a AJAX
-- [x] Normalizacion: Devolver un array de peliculas y no un objeto con peliculas. 
-- [x] Añadir CORS a las rutas de la /api/ directamente o con el middelware cors (Npm) y Helmet para seguridad
-- [ ] Refactorizar todo y darle un último repasito
-- [ ] Mejorar el Readme
-- [ ] Normalizacion: OMBD data -> Title a title y cambio de comportamiento en el front
-- [ ] Mejorar la gestión de errores con un middelware propio (Hablar con @Ulises)
-*/
-
 app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(cors())
