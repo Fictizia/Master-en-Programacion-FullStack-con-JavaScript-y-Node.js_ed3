79b3c243c18f39f266bf162f7dec3a9967d5b584
KoolTheba <26543236+KoolTheba@users.noreply.github.com>
2019-06-25 11:32:10 +0200

    new model DB

--- Teba/MovieFire_ApiRest_SQL/models/db.js
+++ Teba/MovieFire_ApiRest_SQL/models/db.js
@@ -1,60 +1,78 @@
 const mysql = require('mysql');
-// Inicializar la BBDD
-const connectionDB = mysql.createConnection({
-    host: "localhost",
-    user: "yourusername",
-    password: "yourpassword",
-    database: "MovieFire"
-  });
-  
-  connectionDB.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    con.query("CREATE DATABASE IF NOT EXISTS MovieFire", 
-      function (err, result) {
-        if (err) throw err;
-        console.log("MovieFire database created!");
-      });
-    con.query("CREATE TABLE IF NOT EXISTS movies (movie_id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT), (title VARCHAR(150) NOT NULL), (year INTEGER UNSIGNED NOT NULL DEFAULT 2000), (genre VARCHAR(50)), (director VARCHAR(150)), (actors VARCHAR(500)), (plot TEXT), (language VARCHAR(30)), (awards VARCHAR(100)), (imdbRating INTEGER UNSIGNED)", 
-      function (err, result) {
-        if (err) throw err;
-        console.log("Movies table created!");
-    });
-      // createFilmDB();
-    con.query("INSERT INTO movies (title, year, genre, director, actors, plot, language, awards, imdbRating) VALUES ('${Title}', '${Year}', '${Genre}', '${Director}', '${Actors}', '${Plot}', '${Language}', '${Awards}', '${imdbRating}')", 
-    function (err, result) {
-      if (err) throw err;
-      console.log("Movies table created!");
-    });
-    // editFilmDetailsDB();
-    con.query("UPDATE INTO movies(title) SET title = '${newTitle}' WHERE title = '${title}'", 
-    function (err, result) {
-      if (err) throw err;
-      console.log("Film new title changed!");
-    });
-    // filmDetailsDB();
-    con.query("SELECT title, genre, directors, actors, plot FROM movies WHERE title LIKE '${title}'", 
-    function (err, result) {
-      if (err) throw err;
-      console.log("Check the film details!");
-    });
-    // deleteFilmDB();
-    con.query("DELETE FROM movies WHERE title = '${title}'", 
-    function (err, result) {
-      if (err) throw err;
-      console.log("Film deleted!");
-    });
-    // getAllFilmsDB(SELECT title, genre, directors, actors, plot FROM movies limit 10);
-    con.query("SELECT title, genre, directors, actors, plot FROM movies limit 10", 
-    function (err, result) {
-      if (err) throw err;
-      console.log("Film list!");
-    });
-  });
+const {db} = require('../config')
+const connection = mysql.createConnection(db);
+
+connection.connect();
+
+function getMovieDetailsDB(id){
+    return new Promise((resolve, reject) => {
+        const queryText = `SELECT * FROM movies WHERE movie_id = ${id}`
+        connection.query(queryText, (error, results) => {
+            if (error) {
+                console.log(error)
+                return reject(error);
+            };
+            if(results.length === 0){
+                return reject({msg: "Movie not found!"})
+            }
+            resolve(results[0]);
+        });
+
+    })
+}
+
+function getAllMoviesDB(){
+    return new Promise((resolve, reject) => {
+        connection.query('SELECT * FROM movies', (error, results) => {
+            if (error) {
+                console.log(error)
+                return reject(error);
+            };
+            resolve(results);
+          });
+    })
+}
+
+function editMovieDB(id, newTitle){
+    return new Promise((resolve, reject) => {
+        const queryText = `UPDATE movies SET title = '${newTitle}' WHERE movie_id = ${id}`;
+        connection.query(queryText, (error, results) => {
+            if (error) {
+                console.log(error)
+                return reject(error);
+            };
+            resolve(results);
+          });
+    })
+}
+
+function createMovieDB(data){
+    return new Promise((resolve, reject) => {
+        const columns = ["actors", "awards", "director", "genre", "imdbRating", "language", "plot", "title", "year"].sort()
+        const values = columns.map(column => data[column]);
+        const queryText = `INSERT INTO movies (??) VALUES (?)`
+        connection.query(queryText, [columns, values], (err, result) => {
+            if (err) {
+             console.log(err)
+             return reject(err);
+            }
+            resolve(result);
+        });
+    })
+}
+
+function deleteMovieDB(id){
+    return new Promise((resolve, reject) => {
+        const queryText = `DELETE FROM movies WHERE movie_id = ${id}`;
+        connection.query(queryText, (err, result) => {
+            if (err){
+                console.log("Error:", err)
+                return reject(err);
+            }
+            resolve(result);
+        });
+    })
+}
 
 
-
-// Exportar las funciones CRUD
-// module.exports: {}
-// Export for api.js
-module.exports = { getAllFilmsDB, filmDetailsDB, createFilmDB, editFilmDetailsDB, deleteFilmDB }
+module.exports = { getMovieDetailsDB, getAllMoviesDB, editMovieDB, createMovieDB, deleteMovieDB }
