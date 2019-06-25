05656b9e0c5111e6a70dd9d98e163601dd3844d5
KoolTheba <26543236+KoolTheba@users.noreply.github.com>
2019-06-25 11:31:56 +0200

    omdb clean up format

--- Teba/MovieFire_ApiRest_SQL/lib/omdb.js
+++ Teba/MovieFire_ApiRest_SQL/lib/omdb.js
@@ -1,10 +1,34 @@
 const got = require('got')
-const {omdbKey} = require('./config');
+const {omdbKey} = require('../config');
 
 function gotMovieInfo (title){
-    const url = `http://www.omdbapi.com/?i=${title}&apikey=${omdbKey}`;
+    const url = `http://www.omdbapi.com/?t=${title}&apikey=${omdbKey}`;
     return got(url)
-    .then(res => JSON.parse(res.body))
+    .then(res => {
+        const dataRaw = JSON.parse(res.body)
+        if(dataRaw.Error){
+            return Promise.reject(dataRaw.Error)
+        }
+        const data = {}
+        const properties = {
+            "Actors": "actors", 
+            "Awards": 'awards', 
+            "Director": 'director', 
+            "Genre": 'genre', 
+            "imdbRating": 'imdbRating', 
+            "Language": 'language', 
+            "Plot": 'plot', 
+            "Title": 'title', 
+            "Year": 'year'
+        }
+        
+        const dicProps = Object.keys(properties);        
+        dicProps.forEach(prop => {
+            data[properties[prop]] = dataRaw[prop]
+        })
+        
+        return data
+    })
 }
 
 module.exports = { gotMovieInfo }
